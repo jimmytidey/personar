b7ed28390bb4459d1e4fd0a69aad4bcf65925e5e
Jimmy Tidey <jimmy@Jimmys-Air.cust.communityfibre.co.uk>
2023-09-12 16:20:27 +0100

    adding drop box to select region

--- API/app.py
+++ API/app.py
@@ -12,43 +12,41 @@ from data.headshots.get_headshot_urls import get_headshot_urls
 app = Flask(__name__)
 CORS(app,resources={r"/sample/": {"origins": "*"}})
 
+def DfToResponse(df): 
+    json_data = df.to_json(orient='records')
+    print(json_data)
+    response = app.response_class(
+        response=json_data,
+        status=200,
+        mimetype='application/json'
+    )
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 @app.route('/sample/', methods=['GET'])
 
 def sample():
-    sample_df = sample_core_demography_by_ltla(10)
-    print(sample_df)
+    region = request.args.get('region')
+    sample_df = sample_core_demography_by_ltla(10, region)
+
     def add_employment_data(row):
         return sample_employment_by_demography(row['ltla_code'], row['age'], row['ethnicity_code'])
 
     sample_df['employment'] = sample_df.apply(add_employment_data, axis=1)
     sample_df = get_headshot_urls(sample_df)
 
-    json_data = sample_df.to_json(orient='records')
-    print(json_data)
-    response = app.response_class(
-        response=json_data,
-        status=200,
-        mimetype='application/json'
-    )
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    response = DfToResponse(sample_df)
     return response
 
 @app.route('/list_ltlas/', methods=['GET'])
 def list_ltlas():
-    result = list_all_ltlas()
- 
-    json_data = result.to_json(orient='records')
-    print(json_data)
-    response = app.response_class(
-        response=json_data,
-        status=200,
-        mimetype='application/json'
-    )
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    result_df = list_all_ltlas()
+    response = DfToResponse(result_df)
     return response
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8000, debug=True)
+
 
 
 
--- API/data/core_demography_by_ltla/core_demography_by_ltla.py
+++ API/data/core_demography_by_ltla/core_demography_by_ltla.py
@@ -2,29 +2,44 @@ import pandas as pd
 import random
 import os
 
+def is_interactive():
+    import __main__ as main
+    return not hasattr(main, '__file__')
+
 
 # https://www.ons.gov.uk/datasets/RM200/editions/2021/versions/1/filter-outputs/3a155485-7746-4c70-9348-0c8de43d09e6#get-data
 # gender - two categories 
 # age - nine categories
 # ethnicity - 20 cateogies 
 
-def sample_core_demography_by_ltla(sample_size):
+def sample_core_demography_by_ltla(sample_size, region):
 
-    path = os.path.abspath(os.path.dirname(__file__))
-    file_path = os.path.join(path, 'RM032-2021-1-filtered-2023-09-02T13_43_40Z.csv')
-    df = pd.read_csv(file_path)
+    if (is_interactive()):
+        print('interactive')
+        file_path = 'RM032-2021-1-filtered-2023-09-02T13_43_40Z.csv'
+    else:
+        print('not interactive')
+        path = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(
+            path, 'RM032-2021-1-filtered-2023-09-02T13_43_40Z.csv')
+
+    csv_df = pd.read_csv(file_path)
 
     rename_dict = {'Lower tier local authorities Code': 'ltla_code',
-                    'Lower tier local authorities': 'ltla_name',
-                    'Age (9 categories)': 'age_category',
-                    'Ethnic group (20 categories)': 'ethnicity',
-                    'Sex (2 categories)': 'sex',
-                    'Observation': 'observation',
-                    'Ethnic group (20 categories) Code': 'ethnicity_code'}
+                   'Lower tier local authorities': 'ltla_name',
+                   'Age (9 categories)': 'age_category',
+                   'Ethnic group (20 categories)': 'ethnicity',
+                   'Sex (2 categories)': 'sex',
+                   'Observation': 'observation',
+                   'Ethnic group (20 categories) Code': 'ethnicity_code'}
 
-    # call rename () method
-    df.rename(columns=rename_dict,
-                        inplace=True)  
+    csv_df.rename(columns=rename_dict,
+                  inplace=True)
+
+    if (region == 'none'):
+        region_df = csv_df
+    else:
+        region_df = csv_df[csv_df['ltla_code'] == region]
 
     def age_category_to_age(age_category):
         if (age_category == 'Aged 4 years and under'):
@@ -48,14 +63,16 @@ def sample_core_demography_by_ltla(sample_size):
         else:
             return -1
 
-    df['age'] = df['age_category'].apply(age_category_to_age)     
-    df = df[(df['age'] > 16)]
+    region_df['age'] = region_df['age_category'].apply(age_category_to_age)     
+    region_df = region_df[(region_df['age'] > 16)]
 
-    sample_df = df.sample(n=sample_size, weights='observation')
+    sample_df = region_df.sample(n=sample_size, weights='observation')
 
     sample_df['sex'] = sample_df['sex'].apply(str.lower)
     
     sample_df.drop(['Age (9 categories) Code', 'Sex (2 categories) Code'], inplace=True, axis=1)
+    
+    print(sample_df)
 
     return sample_df
 
@@ -69,8 +86,11 @@ def list_all_ltlas():
 
     list = list[['Lower tier local authorities Code',
                  'Lower tier local authorities']]
+    
+    list.rename(columns={'Lower tier local authorities Code': 'ltla_code',
+                 'Lower tier local authorities': 'ltla_name'},inplace=True)
 
-    list.sort_values('Lower tier local authorities', inplace=True)
+    list.sort_values('ltla_name', inplace=True)
 
     return list
 
